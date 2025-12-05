491b0860a715230bcee044135396a992046b1b94
dkoehl595-i <anna.s.725620561@rambler.ru>
2025-12-05 10:06:24 +0800

    add components-export-responsive.py, IDecryptionOracle__factory.ts, fixture.rst

---
+++ database-components-pages/fixture.rst
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\database-components-pages\fixture.rst
+// Generated at: 2025-10-25T05:15:14.315Z
---
+++ frontend/web/src/utils-factory/components-export-responsive.py
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\frontend\web\src\utils-factory\components-export-responsive.py
+// Generated at: 2025-10-25T05:15:14.320Z
